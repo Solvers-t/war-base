42c81d4ff51ec6b2af7904b0788e518fd878e8da
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-09-14 22:08:45 -0300

    Feat: Create estruture to multiples games

--- server.js
+++ server.js
@@ -1,43 +1,25 @@
-import express from 'express'
-import http from 'http'
-import createGame from './public/game.js'
-import socketio from 'socket.io'
+import express from "express";
+import http from "http";
+import socketio from "socket.io";
 
-const app = express()
-const server = http.createServer(app)
-const sockets = socketio(server)
+const app = express();
+const server = http.createServer(app);
+const sockets = socketio(server);
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-const game = createGame()
-game.start()
+sockets.on("connection", (socket) => {
+  const playerId = socket.id;
+  console.log(`> Player connected: ${playerId}`);
 
-game.subscribe((command) => {
-    console.log(`> Emitting ${command.type}`)
-    sockets.emit(command.type, command)
-})
-
-sockets.on('connection', (socket) => {
-    const playerId = socket.id
-    console.log(`> Player connected: ${playerId}`)
-
-    game.addPlayer({ playerId: playerId })
-
-    socket.emit('setup', game.state)
-
-    socket.on('disconnect', () => {
-        game.removePlayer({ playerId: playerId })
-        console.log(`> Player disconnected: ${playerId}`)
-    })
-
-    socket.on('move-player', (command) => {
-        command.playerId = playerId
-        command.type = 'move-player'
-        
-        game.movePlayer(command)
-    })
-})
+  socket.on("disconnect", () => {
+    console.log(`> Player disconnected: ${playerId}`);
+  });
+  socket.on("a",(a) =>{
+    console.log(a);
+  })
+});
 
 server.listen(3000, () => {
-    console.log(`> Server listening on port: 3000`)
-})
+  console.log(`> Server listening on port: 3000`);
+});
