dd248659bc1c078b06bccf7a7fa4fdef4d4b4c80
Jeferson Alves <49992182+desenjeferson@users.noreply.github.com>
2020-09-19 12:15:46 -0300

    Feat: create conection of client and server to find or create games

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import socketio from "socket.io";
 const app = express();
 const server = http.createServer(app);
 const sockets = socketio(server);
+const games = [];
 
 app.use(express.static("public"));
 
@@ -15,9 +16,46 @@ sockets.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log(`> Player disconnected: ${playerId}`);
   });
-  socket.on("a",(a) =>{
-    console.log(a);
-  })
+
+  socket.on("joinGmae", ({ playerId, gameKey }) => {
+    console.log("joinGmae", { playerId, gameKey });
+    for (const game of games) {
+      if (game.gameKey == gameKey) {
+        game.players.push(playerId);
+      }
+    }
+    emitGame(gameKey);
+    console.log(games[gameKey - 1]);
+  });
+
+  socket.on("createGame", (playerId) => {
+    console.log("createGame", playerId);
+    const gameKey = games.length + 1;
+    games.push({ host: playerId, players: [playerId], gameKey });
+    emitGame(gameKey);
+    console.log(games[gameKey - 1]);
+  });
+
+  socket.on("findGame", (playerId) => {
+    console.log("findGame", playerId);
+    let gameKey;
+    for (const game of games) {
+      if (game.players.length = 1) {
+        gameKey = game.gameKey;
+      }
+    }
+    games[gameKey - 1].players.push(playerId);
+    emitGame(gameKey);
+    console.log(games[gameKey - 1]);
+  });
+
+  socket.on("changeGameTag", ({ playerId, gamerTag }) => {
+    console.log("changeGameTag", { playerId, gamerTag });
+  });
+
+  function emitGame(gameKey) {
+    socket.emit("setup", games[gameKey - 1]);
+  }
 });
 
 server.listen(3000, () => {
